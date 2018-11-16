f8ed88cde12a203076b6cd19103c0c7ca68f5752
peijipe <honda@k--s.com>
2018-11-16 19:16:59 +0900

    edit_mainPage

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,7 +4,4 @@ from . import views
 app_name = 'mainpage'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact', views.contact, name='contact'),
-    path('contact/confirm', views.contact, name='confirm'),
-    path('contact/thanks', views.thanks, name='thanks'),
 ]
